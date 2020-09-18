9922618b46203b958b363e634c758bcdb6e87e1f
EugW <wug.bond@gmail.com>
2020-09-18 22:27:58 +0300

    add havoc strings

--- havoc_greatlte.mk
+++ havoc_greatlte.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="greatltexx-user 8.0.0 R16NW N950FXXU3CRC1 release-keys"
 
 BUILD_FINGERPRINT := samsung/greatltexx/dreamlte:8.0.0/R16NW/N950FXXU3CRC1:user/release-keys
+
+HAVOC_MAINTAINER := EugW
+HAVOC_GROUP_URL := https://t.me/havoc8895
